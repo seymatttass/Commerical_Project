5ac6acbf973d8d40475ca3450564dfc19bf50be7
Şeyma Taş <126778789+seymatttass@users.noreply.github.com>
2025-02-28 14:07:12 +0300

    productcategory creatd commit

---
+++ ProductCategory/service/IProductCategoryService.cs
@@ -0,0 +1,13 @@
+﻿using ProductCategory.DTOS.ProductCategory;
+
+namespace ProductCategory.service
+{
+    public interface IProductCategoryService
+    {
+        Task<IEnumerable<Data.Entities.ProductCategory>> GetAllAsync();
+        Task<Data.Entities.ProductCategory> GetByIdAsync(int id);
+        Task<Data.Entities.ProductCategory> AddAsync(CreateProductCategoryDTO createProductCategoryDto);
+        Task<bool> UpdateAsync(UpdateProductCategoryDTO updateProductCategoryDto);
+        Task<bool> DeleteAsync(int id);
+    }
+}
