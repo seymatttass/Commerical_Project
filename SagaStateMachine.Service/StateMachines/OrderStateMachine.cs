80a39abecd02a981d38214f3256df56755c16e0b
Mehmet Eren <129990630+Mehmteren@users.noreply.github.com>
2025-03-06 13:49:18 +0300

    Created SagaStateMachine

---
+++ SagaStateMachine.Service/StateMachines/OrderStateMachine.cs
@@ -0,0 +1,14 @@
+﻿using MassTransit;
+using SagaStateMachine.Service.StateInstances;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SagaStateMachine.Service.StateMachines
+{
+    public class OrderStateMachine : MassTransitStateMachine<OrderStateInstance>
+    {
+    }
+}
---
+++ SagaStateMachine.Service/StateMap/OrderStateMap.cs
@@ -0,0 +1,26 @@
+﻿using MassTransit;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SagaStateMachine.Service.StateInstances;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SagaStateMachine.Service.StateMap
+{
+    public class OrderStateMap : SagaClassMap<OrderStateInstance>
+    {
+        protected override void Configure(EntityTypeBuilder<OrderStateInstance> entity, ModelBuilder model)
+        {
+            entity.Property(x => x.UserId)
+                .IsRequired();
+            entity.Property(x => x.OrderId)
+                .IsRequired();
+            entity.Property(x => x.TotalPrice)
+                .HasDefaultValue(0);
+        }
+    }
+}
