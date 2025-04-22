0098dca53489588d69b5ccbcfeac82fbd92fcb6c
Şeyma Taş <126778789+seymatttass@users.noreply.github.com>
2025-04-22 09:00:33 +0300

    add gatewayapi product

---
+++ AuthAPI/Controllers/AuthsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace AuthAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthsController : ControllerBase
+    {
+        IConfiguration _configuration;
+        public AuthsController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet] // Bu satırı ekleyin
+        public IActionResult Login(string userName, string password)
+        {
+            TokenHandler._configuration = _configuration;
+            return Ok(userName == "seyma" && password == "12345" ? TokenHandler.CreateAccessToken() : new UnauthorizedResult());
+        }
+    }
+}
